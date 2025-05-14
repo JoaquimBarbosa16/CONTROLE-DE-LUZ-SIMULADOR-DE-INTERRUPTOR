a0eb3814c99bd2738370fd03ab449ffd65af7734
JoaquimBarbosa16 <164923566+JoaquimBarbosa16@users.noreply.github.com>
2025-05-14 15:42:04 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const lamp = document.getElementById('lamp');
 const toggleButton = document.getElementById('toggleButton');
 const counterElement = document.getElementById('counter');
+const room = document.getElementById('room');
 
 let isLightOn = false;
 let counter = 0;
@@ -10,20 +11,19 @@ toggleButton.addEventListener('click', () => {
 
   setTimeout(() => {
     if (!isLightOn) {
-      // Ligar
       lamp.src = 'img/lampada ligada.png';
       lamp.alt = 'Lâmpada acesa';
       toggleButton.textContent = 'Desligar';
       counter++;
       counterElement.textContent = counter;
+      room.classList.add('lit');
     } else {
-      // Desligar
       lamp.src = 'img/lampada_apagada.png';
       lamp.alt = 'Lâmpada apagada';
       toggleButton.textContent = 'Ligar';
+      room.classList.remove('lit');
     }
 
-    // Garante fade-in
     lamp.onload = () => lamp.classList.remove('fade-out');
     setTimeout(() => lamp.classList.remove('fade-out'), 300);
 
